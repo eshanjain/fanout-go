318c3d2404a552247a8b91e1ba1882d354c1c828
eshanjain <eshanjain1000@gmail.com>
2024-12-27 09:11:00 -0800

    Create calculator.go

---
+++ calculator.go
@@ -0,0 +1,26 @@
+// calculator.go
+package calculator
+
+// Add returns the sum of two numbers
+func Add(a, b float64) float64 {
+    return a + b
+}
+
+// Subtract returns the difference between two numbers
+func Subtract(a, b float64) float64 {
+    return a - b
+}
+
+// Multiply returns the product of two numbers
+func Multiply(a, b float64) float64 {
+    return a * b
+}
+
+// Divide returns the quotient of two numbers
+// If b is 0, it returns 0 to avoid division by zero
+func Divide(a, b float64) float64 {
+    if b == 0 {
+        return 0
+    }
+    return a / b
+}
