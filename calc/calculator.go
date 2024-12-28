f9ca21ea459045f5b231b0931a16bf86d64bfc17
eshanjain <eshanjain1000@gmail.com>
2024-12-27 21:24:57 -0800

    Update calculator.go

--- calc/calculator.go
+++ calc/calculator.go
@@ -8,8 +8,8 @@ import (
 
 // Add returns the sum of two numbers
 func Add(a, b float64) float64 {
-    fmt.Println(splitByNewLine("Hello World"))
-    fmt.Println(splitByNewLine("Hello World\r\nNew World"))
+    fmt.Println(strings.join(splitByNewLine("Hello World"), ","))
+    fmt.Println(strings.join(splitByNewLine("Hello World\r\nNew World"), ","))
     return a + b
 }
 
